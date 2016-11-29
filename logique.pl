c6853a2b644b24e2cb813e091235fb429ae4ccc5
mulhauser <mulhauser.remy@gmail.comr>
2016-11-29 20:31:06 +0100

    accent

--- logique.pl
+++ logique.pl
@@ -1,18 +1,18 @@
 % Mulhauser remy
-%     zaz  Wolkowicz michel
+% Wolkowicz michel
 
 :- op(20,xfy,?=).
 
-% PrŽdicats d'affichage fournis
+% Prédicats d'affichage fournis
 
-% set_echo: ce prŽdicat active l'affichage par le prŽdicat echo
+% set_echo: ce prédicat active l'affichage par le prédicat echo
 set_echo :- assert(echo_on).
 
-% clr_echo: ce prŽdicat inhibe l'affichage par le prŽdicat echo
+% clr_echo: ce prédicat inhibe l'affichage par le prédicat echo
 clr_echo :- retractall(echo_on).
 
-% echo(T): si le flag echo_on est positionnŽ, echo(T) affiche le terme T
-%          sinon, echo(T) rŽussit simplement en ne faisant rien.
+% echo(T): si le flag echo_on est positionné, echo(T) affiche le terme T
+%          sinon, echo(T) réussit simplement en ne faisant rien.
 
 echo(T) :- echo_on, !, write(T).
 echo(_).
